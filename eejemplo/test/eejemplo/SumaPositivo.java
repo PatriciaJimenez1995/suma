4025cb7b1b385484d0cd8ffea699a74765326ae9
PatriciaJimenez1995 <166231165+PatriciaJimenez1995@users.noreply.github.com>
2024-04-17 11:18:39 +0200

    Add files via upload

---
+++ eejemplo/test/eejemplo/SumaPositivo.java
@@ -0,0 +1,16 @@
+package eejemplo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumaPositivo {
+
+	@Test
+	void test() {
+		System.out.println("Comprovar si la suma  ....");
+		suma s = new suma(4,6);
+		assertTrue(s.sumar()== 10);
+	}
+
+}
